8bfcb337bf166669474f64c31b13faba84fbe1b8
Lechiffre2110 <73186756+Lechiffre2110@users.noreply.github.com>
2023-03-29 14:16:31 +0200

    [KBE-64] feature: clear cart after checkout

--- src/pages/payment/completion.js
+++ src/pages/payment/completion.js
@@ -1,5 +1,32 @@
-function Completion(props) {
-    return <h1>Thank you! 🎉</h1>;
+import jwtDecode from "jwt-decode";
+import {useEffect} from "react";
+
+const Completion = () => {
+    const decodedJwtToken = jwtDecode(localStorage.getItem('jwt-token'));
+    const name = decodedJwtToken.firstName;
+
+    useEffect(() => {
+        clearCart();
+    }, []);
+
+    const clearCart = () => {
+        fetch('http://localhost:28080/api/v1/cart/user/' + decodedJwtToken.userid, {
+            method: 'DELETE',
+        })
+    }
+
+    return (
+        <div className="book-background">
+            <div className="completion-container glowing-border">
+                <h1>Thanks for your order {name}! 🎉</h1>
+                <div>
+                    <img src="https://media.tenor.com/_4xCiEhhoZsAAAAM/dog-smile.gif" alt="test image" />
+                </div>
+                <h2>Have fun reading :)</h2>
+            </div>
+        </div>
+
+    );
 }
 
 export default Completion;
